4be9c2f0ca20f0fd8486968b106e837f44930fe6
malikasinger <malikasinger@gmail.com>
2015-07-09 11:18:30 -0700

    complete, now trying to add firebase

--- components/update/update.js
+++ components/update/update.js
@@ -4,11 +4,23 @@
 var app = angular.module("app.update", []);
 
 app.controller("UpdateController", function ($location, dataservice) {
+    var vm = this;
+    var index = dataservice.currentuser;
 
-    this.index = dataservice.currentuser;
     this.updateentry = function () {
 
-        
-    }
+        dataservice.users[index].name = this.name;
+        dataservice.users[index].age = this.age;
+        dataservice.users[index].email = this.email;
+        $location.path("/view");
+    };
+
+
+    (function () {
+        vm.name = dataservice.users[index].name;
+        vm.age = dataservice.users[index].age;
+        vm.email = dataservice.users[index].email;
+
+    })();
 
 });
